b14f05cf8697569e277a588179ae0e9e173e125c
ivanapizzo <ivb.pizzo@gmail.com>
2023-04-03 19:19:42 +0200

    commit while p.2

--- buclesWhile.js
+++ buclesWhile.js
@@ -1,25 +1,30 @@
 var encontrarPar = false;
-function hasEven(myNums1) {
+function hasEven(myNums) {
     var i = 0;
-    while (i < myNums1.length && !encontrarPar) {
-        if (encontrarPar = (myNums1[i] % 2 == 0)) {
+    // let arrPar = [];
+    while (i < myNums.length) {
+        if (myNums[i] % 2 == 0) {
             return true;
         }
         i++;
     }
+    return false;
 }
 var arr02 = [4, 14, 7, 17, 24, 27];
-hasEven(arr02);
+var arr03 = [3, 5, 9];
 console.log(hasEven(arr02));
-var encontrarM = false;
+console.log(hasEven(arr03));
 function startWithM(myNames) {
     var i = 0;
-    while (i < myNames.length && !encontrarM) {
-        if (encontrarM = (myNames[i][0] == "M")) {
+    while (i < myNames.length) {
+        if (myNames[i][0] == "M") {
             return true;
         }
         i++;
     }
+    return false;
 }
-var arrNombres = ["Margarita", "Milagros", "Maria", "Martin", "Mora"];
+var arrNombres = ["Margarita", "Maria", "Martin", "Mora"];
+var arrNombres02 = ["Andrés", "Julian", "Luisina", "Ivana"];
 console.log(startWithM(arrNombres));
+console.log(startWithM(arrNombres02));
--- buclesWhile.ts
+++ buclesWhile.ts
@@ -4,46 +4,47 @@ let encontrarPar = false;
 function hasEven (myNums:number[]) {
 
     let i = 0;
-    let arrPar = [];
+    // let arrPar = [];
     
-    while(i < myNums.length && !encontrarPar) {
+    while(i < myNums.length) {
 
-        if (encontrarPar = (myNums[i] % 2 == 0)){
+        if (myNums[i] % 2 == 0){
 
             return true;
         }
 
         i++;
-    }  
+    }  return false;
 } 
 
 let arr02 = [4, 14, 7, 17, 24, 27];
+let arr03 = [3, 5, 9];
 
-hasEven(arr02);
+
 console.log(hasEven(arr02));
+console.log(hasEven(arr03));
 
 
 
-let encontrarM = false;
 
 function startWithM(myNames:string[]){
 
     let i = 0;
 
-    while (i < myNames.length && !encontrarM){
+    while (i < myNames.length){
 
 
-        if (encontrarM = (myNames[i][0] == "M")){
+        if (myNames[i][0] == "M"){
 
             return true;
-
-        }
+        }  
         
-        i++;
-
-    } 
+        i ++;
+    }  return false;
 }
 
-let arrNombres = ["Margarita", "Milagros", "Maria", "Martin", "Mora"];
+let arrNombres = ["Margarita", "Maria", "Martin", "Mora"];
+let arrNombres02 = ["Andrés", "Julian", "Luisina", "Ivana"];
 
 console.log(startWithM(arrNombres));
+console.log(startWithM(arrNombres02));
