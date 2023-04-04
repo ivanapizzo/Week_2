6c7ece333c96a519900a1a485f8e3f939b4d1030
ivanapizzo <ivb.pizzo@gmail.com>
2023-04-04 11:03:33 +0200

    probando commit

---
+++ alegraProyect/point.ts
@@ -0,0 +1,40 @@
+
+
+class Point {
+
+    private x: number;
+    private y: number;
+
+    constructor(x:number, y:number) {
+
+        this.x = x;
+        this.y = y;
+    }
+
+    getX(){
+
+    }
+
+    getY(){
+
+
+    }
+    
+    setX(){
+
+
+    }
+
+    setY(){
+
+
+    }
+
+    toString() {
+
+        return this.x + this.y;
+    }
+}
+
+
+export {Point};
