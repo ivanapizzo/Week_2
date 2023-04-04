898c7fcc055df5886d913b01b4e51d6afd3a0eb2
ivanapizzo <ivb.pizzo@gmail.com>
2023-04-04 12:19:38 +0200

    commit reto1

---
+++ algebraProyect/pointTest.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var point_1 = require("./point");
+var myPoint = new point_1.Point(2334.3242, 4353.343);
+console.log(myPoint.getX());
+console.log(myPoint.getY());
+console.log(myPoint.setX());
+console.log(myPoint.setY());
+console.log(myPoint.toString(3243242.22, 2342.324));
---
+++ algebraProyect/pointTest.ts
@@ -0,0 +1,18 @@
+
+import { Point } from "./point";
+
+
+
+
+let myPoint = new Point(2334.3242, 4353.343);
+
+
+console.log(myPoint.getX());
+console.log(myPoint.getY());
+
+
+console.log(myPoint.setX());
+console.log(myPoint.setY());
+
+
+console.log(myPoint.toString(3243242.22, 2342.324));
